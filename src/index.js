067b20205fc83ff497220ba187f481e28c6cc9af
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-11-11 12:39:36 +0000

    Add branch for displayTasks as list

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import {Controller} from './controller.js';
 import './style.css';
 
 const app = new Controller();
+
+//TODO display local storage as list of tasks on page load, refresh and update on submit.
