1de2e609f4295bd77c5f3efe27d00ba2385ee415
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2025-02-05 16:49:36 +0000

    Add createElement fuction to form.js

--- src/form.js
+++ src/form.js
@@ -1,78 +1,67 @@
 const root = document.getElementById('root');
 
 const newTaskForm = () => {
-    console.log('got this far!');
-    const form = document.createElement('form');
-    form.setAttribute('id', 'newTaskForm');
+
+    function createElement(element, attributes = {}, children = []) {
+        const el = document.createElement(element);
+        Object.entries(attributes).forEach(([key, value]) => {
+            el.setAttribute(key, value);
+        });
+        children.forEach(child => (typeof child === 'string')
+        ? el.appendChild(document.createTextNode(child))
+        : el.appendChild(child));
+        return el
+    }
+    
+    const form = createElement('form',{id: 'newTaskForm'},[])
+    root.appendChild(form);
+    
     const fieldset = document.createElement('fieldset');
-    const legend = document.createElement('legend');
-    legend.textContent = 'New Task';
-
+    form.appendChild(fieldset);
+    
+    const legend = createElement('legend', {}, ['New Task']);
+    fieldset.append(legend);
+    
     const titleWrapper = document.createElement('p');
-    const titleLabel = document.createElement('label');
-    titleLabel.setAttribute('for', 'title');
-
-    const titleInput = document.createElement('input');
-    titleInput.setAttribute('type', 'text');
-    titleInput.setAttribute('name', 'title');
-    titleInput.setAttribute('id', 'title');
-    titleInput.setAttribute('placeholder', 'Task');
-
+    fieldset.appendChild(titleWrapper);
+    const titleLabel = createElement('label', {for: 'title'}, ['Task:']);
+    titleWrapper.appendChild(titleLabel);
+    const titleInput = createElement('input', {
+        type: 'text', 
+        name: 'title', 
+        id: 'title', 
+        placeholder: 'Task Name'});
+    titleLabel.appendChild(titleInput);
+        
     const priorityWrapper = document.createElement('p');
-    const priorityLabel = document.createElement('label');
-    priorityLabel.setAttribute('for', 'priority');
-    priorityLabel.textContent = 'Priority';
-
-    const prioritySelector = document.createElement('select');
-    prioritySelector.setAttribute('name', 'priority');
-    prioritySelector.setAttribute('id', 'priority');
-    prioritySelector.setAttribute('autocomplete', 'off');
-
-    const priorityOption1 = document.createElement('option');
-    priorityOption1.setAttribute('value', 'low');
-    priorityOption1.textContent = 'low';
-    const priorityOption2 = document.createElement('option');
-    priorityOption2.setAttribute('value', 'medium');
-    priorityOption2.textContent = 'medium';
-    const priorityOption3 = document.createElement('option');
-    priorityOption3.setAttribute('value', 'high');
-    priorityOption3.textContent = 'high';
+    fieldset.appendChild(priorityWrapper);
+    const priorityLabel = createElement('label', {for: 'priority'}, ['Priority:']);
+    priorityWrapper.appendChild(priorityLabel);
+    const prioritySelector = createElement('select', {
+        name: 'priority',
+        id: 'priority', 
+        autocomplete: 'off'}, []);
+    priorityWrapper.appendChild(prioritySelector);
+    const priorities = ['low', 'medium', 'high'];
+    priorities.forEach(value => {
+        const option = createElement('option', {value: value}, [value]);
+        prioritySelector.appendChild(option);
+    });
 
     const detailsWrapper = document.createElement('p');
-    const detailsLabel = document.createElement('label');
-    detailsLabel.setAttribute('for', 'details');
-
-    const detailsInput = document.createElement('textarea');
-    detailsInput.setAttribute('id', 'details');
-    detailsInput.setAttribute('name', 'details');
-    detailsInput.setAttribute('rows', 10);
-    detailsInput.setAttribute('cols', 30);
-    detailsInput.setAttribute('placeholder', 'Task details...');
-
-    const submitButton = document.createElement('button');
-    submitButton.setAttribute('type', 'submit');
-    submitButton.textContent = 'SUBMIT';
-
-    titleLabel.appendChild(titleInput);
-    titleWrapper.appendChild(titleLabel);
-
-    prioritySelector.appendChild(priorityOption1);
-    prioritySelector.appendChild(priorityOption2);
-    prioritySelector.appendChild(priorityOption3);
-    priorityWrapper.appendChild(priorityLabel);
-    priorityWrapper.appendChild(prioritySelector);
-
-    detailsLabel.appendChild(detailsInput);
-    detailsWrapper.appendChild(detailsLabel);
-
-    fieldset.append(legend);
-    fieldset.appendChild(titleWrapper);
-    fieldset.appendChild(priorityWrapper);
     fieldset.appendChild(detailsWrapper);
-    fieldset.appendChild(submitButton);
+    const detailsLabel = createElement('label', {for: 'details'}, ['Details:']);
+    detailsWrapper.appendChild(detailsLabel);
+    const detailsInput = createElement('textarea', {
+        id: 'details', 
+        name: 'details', 
+        rows: 10, 
+        cols: 30, 
+        placeholder: 'Task details...'},[]);
+    detailsLabel.appendChild(detailsInput);
 
-    form.appendChild(fieldset);
-    root.appendChild(form);
+    const submitButton = createElement('button', {type: 'submit'}, ['SUBMIT']);
+    fieldset.appendChild(submitButton);
 }
 
 export {newTaskForm}
