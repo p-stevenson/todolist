54dd58f249b14787c020c3b1c91836078effa6dc
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2025-02-10 14:08:13 +0000

    Fix module imports and class instantiation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import {Model} from './model.js';
 import {Controller} from './controller.js';
+import {View} from './view.js';
 import './style.css';
 
-const app = new Controller();
+const model = new Model();
+const controller = new Controller(model);
+const view = new View(controller);
